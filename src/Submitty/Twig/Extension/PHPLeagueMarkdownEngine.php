2f4472ca9331d02e18f74c387ea66d09e10c5063
Jason Ng <116290832+jason490@users.noreply.github.com>
2023-11-03 01:18:26 -0400

    Added twig markdown extension
    Referenced off of aptoma/twig-markdown extension

---
+++ src/Submitty/Twig/Extension/PHPLeagueMarkdownEngine.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Submitty\Twig\Extension;
+
+use League\CommonMark\MarkdownConverter;
+use League\CommonMark\CommonMarkConverter;
+
+/**
+ * PHPLeagueCommonMarkEngine.php
+ *
+ * Maps League\CommonMark\MarkdownConverter to Twig\ Markdown Extension
+ * 
+ *  @author Casey McLaughlin <caseyamcl@gmail.com>
+ */
+class PHPLeagueMarkdownEngine
+{
+    /**
+     * @var \League\CommonMark\MarkdownConverter
+     */
+    private $converter;
+
+    /**
+     * Constructor
+     *
+     * Accepts CommonMarkConverter or creates one automatically
+     *
+     * @param \League\CommonMark\MarkdownConverter $converter
+     */
+    public function __construct(MarkdownConverter $converter = null)
+    {
+        $this->converter = $converter ?: new CommonMarkConverter();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function transform($content)
+    {
+        return $this->converter->convertToHtml($content);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'League\CommonMark';
+    }
+}
